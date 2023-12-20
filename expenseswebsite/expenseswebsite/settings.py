015983b72f489fe27e82132ec7b2a8ad26de5eac
Yashas Khot <ykuk9876@gmail.com>
2023-12-20 16:12:43 +0530

    Update settings.py

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r%ru*%0we$tx-2i1pz7q06(-3j-xi0!@8ya-=qzjv5so^mv#+#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
